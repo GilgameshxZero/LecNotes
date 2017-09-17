1d83eb5c758a766251408e225b712c350d24324a
Raymond <rhuffy@me.com>
2017-09-17 10:58:29 -0400

    last efforts!

---
+++ LecNotes/LecNotes/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  LecNotes
+//
+//  Created by Raymond Huffman on 9/17/17.
+//  Copyright © 2017 Raymond Huffman. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
